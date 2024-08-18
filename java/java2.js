af0fe9587780d2affe21a7a2fa533c545b1fbd9b
Mahmudul Hasan Sifat <142844262+mhsifat2000@users.noreply.github.com>
2024-08-18 13:58:51 +0600

    Create java2.js

---
+++ java/java2.js
@@ -0,0 +1,11 @@
+let  myFrame = document.getElementById('Table2');  
+let  display=1;
+function openConsole() {
+     if (display == 1) {
+          myFrame.style.display='block';
+          display=1;
+     }
+}
+function openWindow(){
+            myFrame.style.display='none';          
+}  
